5bebf3bff2000628c6391ec291af94c6f2b542e8
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2025-12-22 20:42:40 +0530

    added example1

---
+++ Lecture - 1 (Intro to C++)/example1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age = 25;
+    double height = 5.9;
+    char grade = 'A';
+    bool isStudent = true;
+    string name = "Adwaith";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
