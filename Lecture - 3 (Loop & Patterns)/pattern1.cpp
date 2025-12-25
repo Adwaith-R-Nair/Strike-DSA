ecccff09d8c80d076386e791a9efa0c4f6592f92
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2025-12-25 17:32:05 +0530

    patterns

---
+++ Lecture - 3 (Loop & Patterns)/pattern1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int j = 1; j <= 6; j++)
+    {
+
+        for (int i = 1; i <= 5; i++)
+        {
+            cout << i << " ";
+        }
+
+        cout << endl;
+    }
+}
---
+++ Lecture - 3 (Loop & Patterns)/pattern2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int j = 1; j <= 5; j++)
+    {
+
+        for (char i = 'a'; i <= 'e'; i++)
+        {
+            cout << i << " ";
+        }
+
+        cout << endl;
+    }
+    cout << endl;
+    for (int i = 1; i <= 5; i++)
+    {
+
+        cout << "Hello Coder Army";
+        cout << endl;
+    }
+}
