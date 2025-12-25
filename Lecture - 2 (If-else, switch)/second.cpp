58cc7a753d80b8a2bed68a911506d03e41bcc9b6
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2025-12-25 15:01:24 +0530

    marks

--- Lecture - 2 (If-else, switch)/second.cpp
+++ Lecture - 2 (If-else, switch)/second.cpp
@@ -14,4 +14,33 @@ int main()
     // else{
     //     cout<<"Your are not eligible to vote";
     // }
+
+    int marks;
+
+    cout<<"Enter your marks: ";
+    cin>>marks;
+
+    if(marks>33){
+        cout<<"You have passed"<<endl;
+    }
+    else{
+        cout<<"You have failed"<<endl;
+    }
+   
+    //  if hoga ek hi else hoga, elseif multiple ho sakte hai
+    if(marks>90){
+        cout<<"Grade: A+";
+    }
+    else if(marks>80&&marks<=90){
+        cout<<"Grade: A";
+    }
+    else if(marks>70&&marks<=80){
+        cout<<"Grade: B+";
+    }
+    else if(marks>60&&marks<=70){
+        cout<<"Grade: B";
+    }
+    else{
+        cout<<"Grade: C";
+    }
 }
