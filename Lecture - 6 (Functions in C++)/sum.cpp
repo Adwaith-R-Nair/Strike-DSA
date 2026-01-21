ea9f090d29e32391a1f9c1c2f4da12b79312b530
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-01-21 15:01:00 +0530

    sum

---
+++ Lecture - 6 (Functions in C++)/sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void sum(int n1, int n2){
+    int sum = n1 + n2;
+    cout << sum << endl;
+}
+
+int main(){
+    sum(4,5);
+    sum(10,13);
+    sum(15,55);
+}
